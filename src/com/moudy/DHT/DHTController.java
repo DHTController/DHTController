7fd0e0fee19e89c81532bb82dedb0625c9c51664
Christian M <Christianmoudy@gmail.com>
2017-01-16 14:33:18 -0500

    Graph ready

--- src/com/moudy/DHT/DHTController.java
+++ src/com/moudy/DHT/DHTController.java
@@ -4,6 +4,14 @@ public class DHTController {
 
 	public static void main(String[] args) {
 		System.out.println("DHT Controller");
+		
+		for(int i = 0; i <= 10; i++) {
+			System.out.println("|");
+		}
+		
+		for(int i = 0; i <= 20; i++) {
+			System.out.print("-");
+		}
 
 	}
 
